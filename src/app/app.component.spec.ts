623c6cbc6dd67f294158aaffd1220befe518e019
agussalberdi <agustinalberdi1@gmail.com>
2019-06-18 20:29:31 -0300

    app component tested

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layouts/header/header.component';
@@ -6,6 +6,9 @@ import { FooterComponent } from './components/layouts/footer/footer.component';
 import { IssueService } from './services/issue.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -21,9 +24,22 @@ describe('AppComponent', () => {
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    expect(component).toBeTruthy();
   });
 
+  it('should have the header component', async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-header')).not.toBe(null);
+  }));
+
+  it('should have the footer component', async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-footer')).not.toBe(null);
+  }));
 });
